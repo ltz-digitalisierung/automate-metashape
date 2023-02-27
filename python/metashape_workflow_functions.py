b82d4fdd5e9cb60b87b54a2b0f0e7862acf90e5b
calculon user <calculongi.t.mlbox@spamgourmet.com>
2023-02-27 16:08:37 +0100

    setting chunk.label to run_name

--- python/metashape_workflow_functions.py
+++ python/metashape_workflow_functions.py
@@ -100,6 +100,7 @@ def project_setup(cfg):
         chunk = doc.addChunk()
         chunk.crs = Metashape.CoordinateSystem(cfg["project_crs"])
         chunk.marker_crs = Metashape.CoordinateSystem(cfg["addGCPs"]["gcp_crs"])
+        chunk.label = run_name
 
     # Save doc doc as new project (even if we opened an existing project, save as a separate one so the existing project remains accessible in its original state)
     doc.save(project_file)
@@ -717,6 +718,5 @@ def finish_run(log_file,config_file):
         file.write("\n\n### CONFIGURATION ###\n")
         documents = yaml.dump(config_full,file, default_flow_style=False)
         file.write("### END CONFIGURATION ###\n")
-
 
     return True
